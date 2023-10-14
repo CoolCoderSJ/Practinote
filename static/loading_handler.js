45f88d125827b88e4bb79ab6a70f75fc0ecbcfb0
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-10-14 12:05:45 -0400

    loading messages

--- static/loading_handler.js
+++ static/loading_handler.js
@@ -1,4 +1,15 @@
 window.onload = () => {
+    let msg = "Loading..."
+    if (window.location.pathname == "/login") {
+        msg = "Generating sample test..."
+    }
+    if (window.location.pathname == "/notion-intermediary" || window.location.pathname == "/docs-intermediary" || window.location.pathname == "/file-intermediary" || window.location.pathname.startsWith("/answers")) {
+        msg = "Generating test..."
+    }
+    if (window.location.pathname.startsWith("/test")) {
+        msg = "Checking answers..."
+    }
+
     document.getElementsByTagName('body')[0].innerHTML += `
     <link rel='stylesheet' href='/static/spinkit.min.css' type='text/css'>
     <div id="loading" class="loading">
@@ -13,6 +24,7 @@ window.onload = () => {
   <div class="sk-grid-cube"></div>
   <div class="sk-grid-cube"></div>
     </div>
+    <h4>${msg}</h4>
     </div><style>
     .loading {
         z-index: 9999;
@@ -25,10 +37,14 @@ window.onload = () => {
         display: none;
         justify-content: center;
         align-items: center;
+        flex-direction: column;
     }
     * {
         --sk-color: #fff;
         --sk-size: 100px;
+    }
+    .loading h4 {
+        background: none;
     }
     </style>`;
 
